8877e7fb0801558050c75bfdcd546c9d81d165a6
Pham Quoc Thinh <phamquocthinh1709@gmail.com>
2026-03-09 19:00:58 +0700

    check

--- lesson 6/test.cpp
+++ lesson 6/test.cpp
@@ -6,21 +6,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n,s; cin >> n >> s;
-    int a[n];
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    int left = 0, right = 0, tong = 0, res = 0;
-    for (;right < n; right++){
-        tong += a[right];
-        while (tong > s){
-            tong -= a[left];
-            left++;
-        }
-        res += right - left + 1;
-    }
-
-    cout << res << endl;
+    cout << "Yes, hallo";
     
     return 0;
 }
